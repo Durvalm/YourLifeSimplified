989a639f6b2cf2161b52a12f578efd20edb843ec
Durval <th3scid@gmail.com>
2022-05-21 19:10:44 -0400

    documented views.py and changed login/singup logic

--- accounts/views.py
+++ accounts/views.py
@@ -5,23 +5,28 @@ from django.contrib import messages
 
 
 def register(request):
+    """Sign Up (register) function"""
+    # Gets input from html signup form
     if request.method == 'POST':
         first_name = request.POST['first_name']
         last_name = request.POST['last_name']
-        username = request.POST['username']
         email = request.POST['email']
         password1 = request.POST['password1']
         password2 = request.POST['password2']
 
+        # If password and re-enter password are equal
         if password1 == password2:
+            # Return error if email already exists
             if User.objects.filter(email=email).exists():
                 messages.error(request, 'The email is already in use!')
                 return redirect('register')
             else:
-                user = User.objects.create_user(username=username, password=password1, email=email, first_name=first_name, last_name=last_name)
+                # if all information is valid, create user
+                user = User.objects.create_user(password=password1, username=email, first_name=first_name, last_name=last_name)
                 user.save()
                 messages.success(request, 'You have successfully signed up!')
                 return redirect('login')
+        # if password and re-enter password are not equal, return error
         else:
             messages.error(request, 'Passwords do not match!')
             return redirect('register')
@@ -30,15 +35,20 @@ def register(request):
 
 
 def login(request):
+    """Login function"""
+    # gets input from html login form
     if request.method == 'POST':
         password = request.POST['password']
-        username = request.POST['username']
+        email = request.POST['email']
 
-        user = auth.authenticate(username=username, password=password)
+        # authenticates if username and password are matching (using email as username)
+        user = auth.authenticate(username=email, password=password)
 
+        # if authenticated, go to home page
         if user is not None:
             auth.login(request, user)
             return redirect('home')
+        # if not authenticated, return back to login page
         else:
             messages.error(request, 'credentials are not valid')
             return redirect('login')
@@ -47,6 +57,7 @@ def login(request):
 
 
 def logout(request):
+    """Logout function"""
     auth.logout(request)
     messages.success(request, 'You are logged out.')
     return redirect('login')
--- todolist/views.py
+++ todolist/views.py
@@ -4,6 +4,7 @@ from .models import ToDoList
 from .forms import ToDoListForm
 from django.core.paginator import Paginator
 from django.utils import timezone
+from django.contrib import messages
 # Create your views here.
 
 
@@ -12,15 +13,18 @@ def todolist(request):
     # Get Objects from ToDoList model and order by deadline date so it displays in todolist.html
     if request.user.is_authenticated:
         tasks = ToDoList.objects.filter(user=request.user).order_by('end_date')
+    # If user is a guest, enter with session key
     else:
         if not request.session.session_key:
             request.session.create()
         tasks = ToDoList.objects.filter(user=None, session_key=request.session.session_key).order_by('end_date')
 
+    # Create a paginator with 9 tasks per page
     paginator = Paginator(tasks, 9)
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
 
+    # Change Task Status to 'Missing' if deadline time has passed
     for task in tasks:
         if not task.is_completed:
             if task.end_date < timezone.now():
@@ -36,11 +40,13 @@ def todolist(request):
 
 def current_tasks(request):
     """This function filters tasks showing only the completed ones"""
+    # Get Objects from ToDoList model and order by deadline date so it displays in todolist/current_tasks
     if request.user.is_authenticated:
         tasks = ToDoList.objects.filter(user=request.user, is_completed=False).order_by('end_date')
+    # If user is a guest, enter with session key
     else:
         tasks = ToDoList.objects.filter(user=None, session_key=request.session.session_key, is_completed=False) .order_by('end_date')
-
+    # Create a paginator with 9 tasks per page
     paginator = Paginator(tasks, 9)
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
@@ -53,11 +59,13 @@ def current_tasks(request):
 
 def completed_tasks(request):
     """This function filters tasks showing only the completed ones"""
+    # Get Objects from ToDoList model and order by deadline date so it displays in todolist/current_tasks
     if request.user.is_authenticated:
         tasks = ToDoList.objects.filter(user=request.user, is_completed=True).order_by('end_date')
+    # If user is a guest, enter with session key
     else:
         tasks = ToDoList.objects.filter(user=None, session_key=request.session.session_key, is_completed=True).order_by('end_date')
-
+    # Create a paginator with 9 tasks per page
     paginator = Paginator(tasks, 9)
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
@@ -72,14 +80,27 @@ def completed_tasks(request):
 def add_task(request):
     """This function is used to add a task from the modal to the database"""
 
+    # Get input from html form
     if request.method == 'POST':
         title = request.POST['title']
         description = request.POST['description']
         end_date = request.POST['deadline']
 
+        # Handle error if date is not included
+        if not end_date:
+            messages.error(request, 'You must include a date')
+            return redirect('todolist')
+
+        # Handle error if title was previously inputted
+        if ToDoList.objects.filter(title=title).exists():
+            messages.error(request, 'You must input a different title')
+            return redirect('todolist')
+
+        # If user is authenticated, get objects from database
         if request.user.is_authenticated:
-            task = ToDoList.objects.create(user=request.user, title=title, description=description, end_date=end_date)
+            task = ToDoList.objects.create(user=request.user, description=description, end_date=end_date, title=title,)
             task.save()
+        # If user is not authenticated, use sessions to handle guest users
         else:
             if not request.session.session_key:
                 request.session.create()
@@ -92,12 +113,17 @@ def add_task(request):
 
 def remove_task(request, id):
     """This function removes tasks from the To-Do-List"""
+    # Get user's data if user is authenticated
     if request.user.is_authenticated:
+        # Get task id from html and delete it
         tasks = ToDoList.objects.get(user=request.user, id=id)
         tasks.delete()
+    # Get guest's data
     else:
+        # If session key doesn't exist, create it
         if not request.session.session_key:
             request.session.create()
+        # Get task id from html and delete it
         tasks = ToDoList.objects.get(user=None, id=id, session_key=request.session.session_key)
         tasks.delete()
 
@@ -106,18 +132,28 @@ def remove_task(request, id):
 
 def edit_task(request, id):
     """This function redirects the user to another page so he can edit info"""
+    # Get user's data if user is authenticated
     if request.user.is_authenticated:
         tasks = ToDoList.objects.get(user=request.user, id=id)
+    # Get guest's data
     else:
         tasks = ToDoList.objects.get(user=None, id=id, session_key=request.session.session_key)
 
+    # Get data input from user in html form
     if request.method == 'POST':
         title = request.POST['title']
         description = request.POST['description']
         end_date = request.POST['deadline']
 
+        # Handle error if title was previously inputted
+        if ToDoList.objects.filter(title=title).exists():
+            messages.error(request, 'You must input a different title')
+            return redirect('todolist')
+
+        # Update task if user is authenticated
         if request.user.is_authenticated:
             ToDoList.objects.filter(id=id).update(user=request.user, title=title, description=description, end_date=end_date)
+        # Update task if user is a guest (not authenticated)
         else:
             if not request.session.session_key:
                 request.session.create()
@@ -133,17 +169,23 @@ def edit_task(request, id):
 
 
 def change_status(request, id):
-    """Changes Task status when user clicks on "current" or "completed" """
+    """Changes Task status when user clicks on 'current' or 'completed' """
+    # If user is authenticated, get data
     if request.user.is_authenticated:
+        # Get task id from html href
         tasks = ToDoList.objects.get(user=request.user, id=id)
+    # if user is guest
     else:
+        # Get task id from html href
         tasks = ToDoList.objects.get(user=None, id=id, session_key=request.session.session_key)
 
+    # If task is completed and change_status button is clicked, change it to Current
     if tasks.is_completed:
         tasks.is_completed = False
         tasks.is_late = False
         tasks.save()
         return redirect('todolist')
+    # If task is current and change_status button is clicked, change it to completed
     else:
         tasks.is_completed = True
         tasks.is_late = False
@@ -153,6 +195,7 @@ def change_status(request, id):
 
 def task_description(request, id):
     """Renders all information of the task on the screen"""
+    # Get task id and send data to task_description.html
     if request.user.is_authenticated:
         task = ToDoList.objects.get(user=request.user, id=id)
     else:
